a58563c35bbb5ff21dec137e5b64a973ab609061
Yaroslav Klimas <yarklim@gmail.com>
2023-06-25 09:37:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
       const allContacts = await contacts.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
     case 'get':
       const oneContact = await contacts.getContactById(id);
-      return console.log(oneContact);
+      return console.table(oneContact);
 
     case 'add':
       const newContact = await contacts.addContact({ name, email, phone });
-      return console.log(newContact);
+      return console.table(newContact);
 
     case 'remove':
       const removeContact = await contacts.removeContact(id);
-      return console.log(removeContact);
+      return console.table(removeContact);
 
     default:
       return console.warn('\x1B[31m Unknown action type!');
